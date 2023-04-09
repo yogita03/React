f21e1394f0d695ff396e0fffc20b971b8d363abc
yogita03 <patilyogita408@gmail.com>
2023-04-09 16:50:41 +0530

    Task 10Change

--- src/components/ExpenseDetails.js
+++ src/components/ExpenseDetails.js
@@ -5,15 +5,18 @@ function ExpenseDetails(props){
        const location = props.location;
 
     return (
-        <div>
+        <>
         <div className="expense-item__description">
             {/* <p> {props.index}</p> */}
             <h2>{title}</h2>
+            </div>
+            <div style={{display: "flex" , justifyContent: "space-around", alignItems: "center", minWidth:"200px"
+            }}>
              <div className="expense-item__text" >
             <h2>{location}</h2></div> 
             <div className="expense-item__price">${amount} </div>
             </div>
-            </div>
+            </>
     );
     
     
