d4c66f9c44e3639bebd6f596d6abb5b8cddb3035
yogita03 <patilyogita408@gmail.com>
2023-04-09 12:34:34 +0530

    Task 9

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import ExpenseItem from './components/ExpenseItem';
+// import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: 'Water'
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12), location: 'Recharge' },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: 'Petrol'
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: 'Cut'
+    },
+  ];
+
+  // const list = {
+
+  
   return (
     // <div className="App">
     //   <header className="App-header">
@@ -18,12 +48,25 @@ function App() {
     //       Learn React
     //     </a>
     //   </header>
-    // </div>
-    <div>
-      <h2>Expense Items!</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
-  );
-}
+      
+      <div>
+      <h2> Expense Tracker</h2>
+     {expenses.map((Val,index) => {
+                return (
+                    <div>
+                        <ExpenseItem title={Val.title} index = {index + 1}
+                            amount={Val.amount}
+                            date={Val.date}
+                            location={Val.location}>
+                             </ExpenseItem>
+                    </div>
+                )
+            })}
 
+
+
+
+    </div>
+    );
+}
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,23 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    // const expenseDate = new Date(2023, 4, 7);
+    // const expenseTitle = 'car Insurance';
+    // const expenseAmount = 294.67;
+    // // const LocationOfExpenditure = 'Petrol';
+    
     return (
-    <div>
-       <div>Expense Items</div>
-        <div>
-            <div>Food Rs 10</div>
-            <div>Petrol Rs 100</div>
-            <div>Movies Rs 200</div>
+    <div className = "expense-item">
+       <div>{props.date.toISOString()}</div>
+        <div className="expense-item__description">
+            <p> {props.index}</p>
+            <h2>{props.title}</h2>
+            <div className="expense-item__text" >
+            <h2>{props.location}</h2></div> 
+            <div className="expense-item__price">${props.amount} </div>
+                
+            
+             
         </div>
     </div>
     );
