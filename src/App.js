36c963f1d4c0e0393aee2e7ef8482e97ada1d641
yogita03 <patilyogita408@gmail.com>
2023-04-11 15:57:09 +0530

    AdReact 2

--- src/App.js
+++ src/App.js
@@ -11,22 +11,22 @@ const App = () => {
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      location: 'Water'
+      location: 'Nagpur'
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12), location: 'Recharge' },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12), location: 'Pune' },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      location: 'Petrol'
+      location: 'Mumbai'
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
-      location: 'Cut'
+      location: 'Hyderabad'
     },
   ];
 
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,41 +1,35 @@
-import  ExpenseDetails from './ExpenseDetails';
+import React, { useState } from 'react';
+// import  ExpenseDetails from './ExpenseDetails';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    // const expenseDate = new Date(2023, 4, 7);
-    // const expenseTitle = 'car Insurance';
-    // const expenseAmount = 294.67;
-    // // const LocationOfExpenditure = 'Petrol';
-    // const clickHandler = () => {
-    //     console.log('Clicked!!!');
-    // }
+  
+    const [amount, setAmount] = useState(props.amount);
     
-    // return (
-      
-    // <Card className = "expense-item"> 
-    //    <ExpenseDate date={props.date} />
-    //     <ExpenseDetails amount={props.amount}  location={props.location}  title={props.title}/>
-       
-    //    <button onClick={clickHandler}>Change Title</button>
-    //    </Card>
-    
-   
-    // );
-
     const clickHandler = () => {
-       let d = document.activeElement.parentElement.parentElement
-       d.remove()
-    }
-        return (
+       
+        setAmount('100');
+        console.log(amount);
+    };
+       return (
           
         <Card className = "expense-item"> 
            <ExpenseDate date={props.date} />
-            <ExpenseDetails amount={props.amount}  location={props.location}  title={props.title}/>
-
-            <button onClick={clickHandler}>Delete Expenses</button>
-       
+           <div className="expense-item__description">
+            {/* <p> {props.index}</p> */}
+            <h2>{props.title}</h2>
+           
+            {/* <div style={{display: "flex" , justifyContent: "space-around", alignItems: "center", minWidth:"200px"
+            }}> */}
+             <div className="expense-item__description" >
+            <h2>{props.location}</h2>
+            <div className="expense-item__price">${amount} </div>
+            </div>
+            </div>
+            {/* </div>  */}
+            <button onClick={clickHandler}>Change Title</button>
          </Card>
        
         );
