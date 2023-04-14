59c1ac03ab798c80f472311717836526bf586bcf
yogita03 <patilyogita408@gmail.com>
2023-04-14 12:07:22 +0530

    AdReact 6

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,24 +3,53 @@ import React, { useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-   const [enterTitle, setEnteredTitle] = useState('');
-   const [enterAmount, setEnteredAmount] = useState('');
-   const [enterDate, setEnteredDate] = useState('');
+   const [enteredTitle, setEnteredTitle] = useState('');
+   const [enteredAmount, setEnteredAmount] = useState('');
+   const [enteredDate, setEnteredDate] = useState('');
+    //   const [userInput, setUserInput]= useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: '',
+    //  });
 
     const titleChangeHandler = (event) => {
       setEnteredTitle(event.target.value);
+    //   setUserInput({
+    //     ...userInput,
+    //     enteredTitle: event.target.value,
+    //   });
     };
 
-    const amountChangehandler = event => {
+    const amountChangehandler = (event) => {
         setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        //   });
     };
 
-    const dateChangeHandler = event => {
+    const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        //   })
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData);
     };
 
     return (
-    <form>
+    <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                <label>Title</label>
@@ -40,7 +69,7 @@ const ExpenseForm = () => {
             </div>
         </div>
         <div className='new-expense__actions'>
-            <button type="submit">Add Expense</button>
+            <button type="submit" >Add Expense</button>
         </div>
     </form>
     );
