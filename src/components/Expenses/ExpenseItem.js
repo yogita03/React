05d35429c5455725a5697d0c29dad09ee73b4f44
yogita03 <patilyogita408@gmail.com>
2023-04-16 10:01:20 +0530

    AdReact 9

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 // import  ExpenseDetails from './ExpenseDetails';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -6,30 +6,27 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
   
-    const [title, setTitle] = useState(props.title);
+    // const [title, setTitle] = useState(props.title);
     
-    const clickHandler = () => {
+    // const clickHandler = () => {
        
-        setTitle('Updated');
-        console.log(title);
-    };
+    //     setTitle('Updated');
+    //     console.log(title);
+    // };
        return (
           
         <Card className = "expense-item"> 
            <ExpenseDate date={props.date} />
            <div className="expense-item__description">
             {/* <p> {props.index}</p> */}
-            <h2>{title}</h2>
-           
-            {/* <div style={{display: "flex" , justifyContent: "space-around", alignItems: "center", minWidth:"200px"
-            }}> */}
+            <h2>{props.title}</h2>
              <div className="expense-item__description" >
             <h2>{props.location}</h2>
             <div className="expense-item__price">${props.amount} </div>
             </div>
             </div>
             {/* </div>  */}
-            <button onClick={clickHandler}>Change Title</button>
+            {/* <button onClick={clickHandler}>Change Title</button> */}
          </Card>
        
         );
