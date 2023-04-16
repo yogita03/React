b7d9861e5ccb4b90e31e6bfe7ec57e2d60ef313f
yogita03 <patilyogita408@gmail.com>
2023-04-16 12:23:57 +0530

    AdReact 11

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,28 +9,54 @@ const Expenses = (props) => {
     
       const [filteredYear, setFilteredYear] = useState('2020');
 
-      const filterChangeHandler = selectedYear => {
+      const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
       };
 
+      const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+      });
+         
+      // let expensesContent = <p>No expenses found.</p>;
+
+      // if(filteredExpenses.length > 0){
+      //   expensesContent =  filteredExpenses.map((expense) => (
+      //     <ExpenseItem 
+      //        key = {expense.id}
+      //         title={expense.title} 
+      //         amount = {expense.amount}
+      //         date = {expense.date}
+      //     />
+      //     ));
+      // }
     return (
+      <div>
         <Card className="expenses">
           <ExpensesFilter 
           selected={filteredYear} 
           onChangeFilter={filterChangeHandler} 
           />
-           {props.items.map(expense =>
-            <ExpenseItem 
-               key = {expense.id}
+           {/* {filteredExpenses.length === 0 &&  */}
+              {/* <p>No expenses found.</p>}  */}
+
+          {/* {filteredExpenses.length > 0 && */}
+          
+           {filteredExpenses.map((expense) => (
+           <ExpenseItem 
+              key = {expense.id}
                 title={expense.title} 
                 amount = {expense.amount}
                 date = {expense.date}
             />
-            )}         
+            
+           ) 
+             )}         
+              {/* {expensesContent} */}
           
         </Card>
+        </div>
       );
-    }
+    };
    
 
 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -79,8 +79,8 @@ const ExpenseForm = (props) => {
                <label>Date</label>
                <input 
                type="date" 
-               min="2023-01-01" 
-               max="2025-12-31" 
+               min="2019-01-01" 
+               max="201-12-31" 
                value={enteredDate} 
                onChange={dateChangeHandler}/> 
             </div>
