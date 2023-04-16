ceac99c4b891a6f28ac12ec5bd216634f8bb0932
yogita03 <patilyogita408@gmail.com>
2023-04-16 22:38:46 +0530

    AdReact 14

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import React, { useState} from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -42,6 +43,7 @@ const Expenses = (props) => {
             
            {/* )  */}
              {/* )}          */}
+             <ExpensesChart expenses={filteredExpenses}/>
             <ExpensesList items={filteredExpenses}/> 
           
         </Card>
